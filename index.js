e368ad0519f0c13b5bd83ead441dd73662d888f0
Cherry <73485099+JustFoxx@users.noreply.github.com>
2021-04-03 09:31:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,10 +34,13 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
     const event = require(`./events/${file}`)
 
-    if (event.once)
-        return client.once(event.name, (...args) => event.execute(...args, client))
+    if (!event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client))
+        
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client))
+    }
 
-    client.on(event.name, (...args) => event.execute(...args, client))
 }
 
 
